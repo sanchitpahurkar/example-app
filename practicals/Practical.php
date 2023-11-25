5e370d4284fa994468c3e43ec5bdaaa6ce835c15
sanchitpahurkar <pahurkarsm@rknec.edu>
2023-11-25 09:39:59 +0530

    Add Practical class

---
+++ practicals/Practical.php
@@ -0,0 +1,30 @@
+<?php
+
+class Practical {
+    // Function to print "Hello, World!"
+    public static function printHello() {
+        echo "Hello, World!";
+    }
+
+    // Function to add two numbers
+    public static function add($num1, $num2) {
+        return $num1 + $num2;
+    }
+
+    // Function to generate Fibonacci sequence
+    public static function generateFibonacciSequence($n) {
+        $sequence = [];
+        $a = 0;
+        $b = 1;
+        
+        for ($i = 0; $i < $n; $i++) {
+            $sequence[] = $a;
+            $temp = $a + $b;
+            $a = $b;
+            $b = $temp;
+        }
+        
+        return $sequence;
+    }
+}
+?>
