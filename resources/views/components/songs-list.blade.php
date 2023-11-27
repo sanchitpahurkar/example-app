5260a226fb31c56e10751b59f7a6f3c97743bfcf
sanchitpahurkar <pahurkarsm@rknec.edu>
2023-11-28 02:40:06 +0530

    Create a new component to render songs list and Created a Playlist page

--- resources/views/components/songs-list.blade.php
+++ resources/views/components/songs-list.blade.php
@@ -4,24 +4,12 @@
         <div class="featured-songs">
             @foreach($songs as $song)
             <div class="song">
-                <h3>Believer</h3>
-                <p>Imagine Dragons</p>
+                <h3>{{ $song['title'] }}</h3>
+                <p>{{ $song['artist'] }}</p>
             </div>
             <div class="song">
-                <h3>Unstoppable</h3>
-                <p>Sia</p>
-            </div>
-            <div class="song">
-                <h3>2002</h3>
-                <p>Anne-Marie</p>
-            </div>
-            <div class="song">
-                <h3>Shape of you</h3>
-                <p>Ed Sheeran</p>
-            </div>
-            <div class="song">
-                <h3>Perfect</h3>
-                <p>Ed Sheeran</p>
+            <h3>{{ $song['title'] }}</h3>
+            <p>{{ $song['artist'] }}</p>
             </div>
             @endforeach
             <!-- Add more featured songs -->
